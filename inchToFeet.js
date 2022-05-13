ac9cbee4621c016a92c09bbf41f1e0ee3891629f
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-05-13 16:10:02 +0600

    first commit

---
+++ inchToFeet.js
@@ -0,0 +1,31 @@
+// 1ta value niye kaj krle aivebe
+
+// var inch = 156;
+// var feet = inch/12;
+// console.log(feet);
+
+// multiple value niye kaj korle function use korte hoy
+
+// ak e kaj bar bar kra lgle take function diye korle easy hoy
+
+// function inchToFeet(inch) {
+//     var feet = inch/12;
+//     return feet;
+// }
+// function re value baire theke set krte chaile, return use krte hoy
+
+// var nanaFeet = inchToFeet(156);
+// console.log(nanaFeet);
+// var dadiFeet = inchToFeet(288);
+// console.log(dadiFeet);
+
+
+// ai kajtai loop diye korte chaile
+// nana, nani, ader k 1ta var er moddhe niye arry te niye gelam
+
+
+var senior = [156, 288, 300];
+
+var nanaFeet = inchToFeet(senior[0]);
+console.log(nanaFeet);
+
