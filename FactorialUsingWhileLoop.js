d8afcad93ef08304cb730b2d58400fcb7f330602
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-05-28 23:43:36 +0600

    fact

---
+++ FactorialUsingWhileLoop.js
@@ -0,0 +1,12 @@
+var i = 1;
+var factorial = 1;
+while (i <= 10) {
+    factorial = factorial * i;
+    i++;
+    // console.log(factorial);
+    // loop er vitore rakhle 1 theke jei porjonto ase sei porjonto dekhabe
+}
+console.log(factorial);
+
+// console log loop er baire rakhle
+// 1ta value dekhabe
