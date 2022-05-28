efc781a038007745ec56228f432ec260887d6f4e
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-05-28 16:30:06 +0600

    factorial

--- FactorialUsingForLoop.js
+++ FactorialUsingForLoop.js
@@ -3,11 +3,11 @@
 // 4! = 1*2*3*4
 // 5! = 1*2*3*4*5
 
-// var factorial = 1;
-// for (var i = 1; i <= 5; i++) {
-//     factorial = factorial * i
-//     console.log(i, factorial);
-// }
+var factorial = 1;
+for (var i = 1; i <= 5; i++) {
+    factorial = factorial * i
+    console.log(i, factorial);
+}
 
 
 // for loop diye factorial ber korake bole
