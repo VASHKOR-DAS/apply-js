d32628a615b4746c7ff7a44c2281830a1a793276
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-05-29 13:59:45 +0600

    FibonacciSeriesRecursive

---
+++ FibonacciSeriesRecursive.js
@@ -0,0 +1,19 @@
+//[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,..]
+
+function fibonacci(n) {
+    if (n == 0) {
+        return [0]
+    }
+    else if (n == 1) {
+        return [0, 1];
+    }
+    else{
+        // calculate array n-th element
+        var fibo = fibonacci(n-1);
+        var nextElement = fibo[n-1] + fibo[n-2];
+        fibo.push(nextElement)
+        return fibo;
+    }
+}
+var result = fibonacci(10);
+console.log(result);
